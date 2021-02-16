498999d9c42fa9c2d44d35ec088685bf7050e6b2
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-16 04:18:13 +0000

    delivered

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
   let pronoun = ['the','our'];
   let adj = ['great', 'big' ];
   let noun = ['jogger','racoon'];
+  let domain = ['com','net','gov','org'];
 
 for(let i = 0;i<pronoun.length;i++) {
 
@@ -11,8 +12,13 @@ for(let j = 0;j<adj.length;j++) {
 for(let k = 0;k<noun.length;k++) {
 
 
-console.log (pronoun[i]+"."+adj[j]+"."+noun[k]+".com")
+for(let l = 0;l<domain.length;l++) {
 
+
+console.log (pronoun[i]+"."+adj[j]+"."+noun[k]+"."+domain[l])
+
+    
+  }
     
   }
 
