d1e6ad799b58f33a8259b1f51aea02ed91bb0abc
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-16 03:58:25 +0000

    delivered

---
+++ app.js
@@ -0,0 +1,40 @@
+  let pronoun = ['the','our'];
+  let adj = ['great', 'big' ];
+  let noun = ['jogger','racoon'];
+  let count = 0
+
+  for (count=0; count>0) {
+
+
+
+for () {
+
+for () {
+
+
+
+
+
+
+
+
+    
+  }
+
+
+
+
+
+
+
+    
+  }
+
+
+
+
+
+
+  }
+
+  console.log(pronoun[0], adj[1], noun[1])
