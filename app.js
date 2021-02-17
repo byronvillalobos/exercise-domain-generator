bc7be8ab56870b12b0b4f9ec80546f4011d36e60
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-17 23:38:45 +0000

    delivered

--- app.js
+++ app.js
@@ -4,28 +4,12 @@
   let domain = ['com','net','gov','org'];
 
 for(let i = 0;i<pronoun.length;i++) {
-
-
-
-for(let j = 0;j<adj.length;j++) {
-
-for(let k = 0;k<noun.length;k++) {
-
-
-for(let l = 0;l<domain.length;l++) {
-
-
-console.log (pronoun[i]+"."+adj[j]+"."+noun[k]+"."+domain[l])
-
-    
-  }
-    
-  }
-
-
-    
-  }
-
-
-  }
+            for(let j = 0;j<adj.length;j++) {
+                for(let k = 0;k<noun.length;k++) {
+                        for(let l = 0;l<domain.length;l++) {
+                        console.log (pronoun[i]+"."+adj[j]+"."+noun[k]+"."+domain[l])
+                        }
+                }
+            }
+}
 
